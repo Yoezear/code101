f86b2a9569344e996353b1eac13198308b6dfb20
thukten yoezear <02230229.cst@rub.edu.bt>
2024-05-24 14:12:27 +0600

    first commit

---
+++ class_activity.py
@@ -0,0 +1,19 @@
+
+name=input("Enter your name: ")
+print(name)
+num1= float(input("Enter the first value: "))
+num2= float(input("Enter the second value: "))
+print("Addition")
+sum= num1 + num2
+print(sum)
+print("Subtraction")
+diff = num1 - num2
+print(diff)
+print("Multiplication")
+product = num1 * num2
+print(product)
+print("Division")
+quo = num1 / num2
+print(quo)
+print("Your answers are all provided.")
+print("Thank you")
